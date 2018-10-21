b2651e2d3880c947807b8a23aa375866429bdba9
Johnathon Wood <jaywood128@gmail.com>
2018-10-21 19:41:23 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,3 +10,14 @@ def line(array)
   puts "#{message}"
   end
 end
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts"Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.shift()}."
+  end
+end
