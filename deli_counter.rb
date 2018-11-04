3b944da74aaca1d7f6712b106f91c92cc1a741d0
Janita Madramootoo <jaymadr@gmail.com>
2018-11-04 20:51:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else array.length > 0
+    phrase = "The line is currently:"
+    position = ""
+    array.each_with_index do |name, index|
+      position << " #{index + 1}. #{name}"
+    end
+    puts phrase += position
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
