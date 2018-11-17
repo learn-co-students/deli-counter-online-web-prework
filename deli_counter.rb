04f794471ca9f2468abf5364d00a1316fc01b099
Nick Inman <nick@theoatmeal.com>
2018-11-17 19:10:49 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli []
+other_deli ["Logan", "Avi", "Spencer"] }
+another_deli ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
+def line(deli)
+  
+  if deli.empty?
+    puts "The line is currently empty."
+  else 
+    print "The line is currently: "
+    deli.each_with_index { |name, position| print " #{position + 1}. #{name} "
+end
