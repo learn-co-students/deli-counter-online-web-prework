608a8aaaf803042cbb2b37077b28c2bca5480c54
s greenbaum <gbs4ever2@gmail.com>
2018-12-18 16:40:47 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,7 @@
 katz_deli = []
 def line (katz_deli)
 
+
 if katz_deli.empty? == true
 puts "The line is currently empty."
 
@@ -11,16 +12,19 @@ else
  puts str
 end
 end
+
 def take_a_number(katz_deli,string)
 katz_deli.push(string)
-katz_deli.each_with_index.map{|name,index|puts "Welcome, #{name}. You are number #{index+1} in line."}
+
+puts "Welcome, #{string }. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli.empty? == true
     puts "There is nobody waiting to be served!"
   else
-    katz_deli.each {|name| puts "Currently serving #{name}."}
+  name=katz_deli.shift  #katz_deli.each {|name|
+puts "Currently serving #{name}."
 
 end
 end
