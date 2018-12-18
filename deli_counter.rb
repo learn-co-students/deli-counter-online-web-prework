6a1ce2cf7f486b86e64a5b89fb0cf8383ad7be0a
s greenbaum <gbs4ever2@gmail.com>
2018-12-18 07:38:38 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,21 +12,15 @@ else
 end
 end
 def take_a_number(katz_deli,string)
-string= katz_deli.push(string)
-string.each_with_index.map{|name,index|puts "Welcome, #{name}. You are number #{index+1} in line."}
+katz_deli.push(string)
+katz_deli.each_with_index.map{|name,index|puts "Welcome, #{name}. You are number #{index+1} in line."}
 end
 
 def now_serving(katz_deli)
   if katz_deli.empty? == true
     puts "There is nobody waiting to be served!"
   else
-#katz_deli.shift
+    katz_deli.each {|name| puts "Currently serving #{name}."}
 
-katz_deli.each {|name |
-name.shift
-
- "Currently serving #{name}."}
-
-puts sign2
 end
 end
