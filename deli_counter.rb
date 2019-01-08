f7b7c2403e070902581f7e7ffd7ca6e5e20486e8
Michael J. Schiumo <mschiumo18@gmail.com>
2019-01-08 02:58:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(katz_deli)
+  i = katz_deli.length 
+  if i == 0 
+    puts "The line is currently empty."
+  elsif i > 0 
+     deli_line = ""
+     katz_deli.each_with_index do |name, index|
+          deli_line += "#{index + 1}. #{name} "
+     end 
+     puts "The line is currently: #{deli_line.strip}"
+  end
+end 
+
+
+
+
+
+
+
+
+def take_a_number(katz_deli, name)
+  new_line = []
+i = katz_deli.length
+if i > 0 
+  puts "Welcome, #{name}. You are number #{index} in line."
+end
+end
+  
+
