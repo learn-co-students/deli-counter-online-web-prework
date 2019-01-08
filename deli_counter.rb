87eb210a50d8acabc25e899234ea633dd4da3c56
A Glazer <avigayilglazer@gmail.com>
2019-01-08 19:05:23 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,33 +1,34 @@
+
 katz_deli = []
 other_deli = ["Logan", "Avi", "Spencer"]
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+
 
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
     message = "The line is currently:"
-    katz_deli.each_with_index {|other_deli, index| message += " #{index+1}. #{other_deli}"}
+
+
+    katz_deli.each_with_index {|ppl, index| message += " #{index+1}. #{ppl}"}
+
     puts message
   end
 end
 
-def take_a_number(katz_deli, other_deli)
-  if katz_deli.size == 0
-    katz_deli << other_deli
-    puts "Welcome, #{other_deli}. You are number 1 in line."
-  else katz_deli.size > 0
-    katz_deli << other_deli
-    num = katz_deli.index(other_deli)+1
-    puts "Welcome, #{other_deli}. You are number #{num} in line."
-  end
+require 'pry'
+def take_a_number(katz_deli, name)
+
+    katz_deli << name
+    num = katz_deli.size
+    puts "Welcome, #{name}. You are number #{num} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
   else
-    num = other_deli.shift
-    puts "Currently serving #{num}."
+    puts "Currently serving #{katz_deli.shift}."
   end
 end
