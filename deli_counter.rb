561c76f0b437c162069a53efae2eca436c7180bd
Lu Daley <daleyluis@gmail.com>
2019-01-23 22:51:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,33 @@
 # Write your code here.
-
+require 'pry'
 def line(katz_deli)
+  #binding.pry
   if katz_deli.length == 0 
     puts "The line is currently empty."
+  elsif katz_deli.length > 0
+     string = "The line is currently:" 
+     katz_deli.each_with_index do |name, index|
+       string << " #{index + 1}. #{name}"
   end
-#  ["Logan", "Avi", "Spencer"]
- #string = "This line is currently: "
-  if katz_deli.length > 0
-     katz_deli.each_with_index do |name,index|
-       puts "The line is currently: #{index + 1}. #{name}"
-  end
+puts string
+end
+end
+
+def take_a_number(katz_deli,name)
+ name = new_person
+ if katz_deli.length == 0 
+     
+     puts "Welcome, #{name}. You are number #{index} in line.")
+#Ada
+end
+#new_person added to end of line/array/last index slot
+#puts new_person name + index # +1 
 
 end
+
+
 #current line = 
-end
+#end
 #end
 
 
