d6f57a16b1eab51801dac594a4672dbb6e2b9654
Lu Daley <daleyluis@gmail.com>
2019-01-23 22:13:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,9 @@ def line(katz_deli)
  #string = "This line is currently: "
   if katz_deli.length > 0
      katz_deli.each_with_index do |name,index|
-    puts "The line is currently: #{index + 1}. #{name}"
+       puts "The line is currently: #{index + 1}. #{name}"
   end
+
 end
 #current line = 
 end
