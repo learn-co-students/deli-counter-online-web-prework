6b495a91c0b67ba5921567f24d0bab3f5f1ac412
Christoph Gruener <christophgruener@me.com>
2019-02-07 23:26:34 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,52 @@
-katz_deli = []
+require 'pry'
+
+#global variable
+$something = 'Hello'
+
+$ticket_number = 0 
+$serving_counter = 1
+$number_in_line = 1
 
 
-def line(katz_deli) 
-  if katz_deli.length > 0 
-    current_line = "The line is currently:"
-    katz_deli.map.with_index { |x, i| current_line << " #{i+1}. #{x}"}
-    puts current_line
-  else
-     puts "The line is currently empty."
-  end
-end
+#def line(katz_deli) 
+#  if $deli_counter > 0 
+#    current_line = "The line is currently:"
+#    katz_deli.map.with_index { |x, i| current_line << " #{i+1}. #{x}"}
+#    puts current_line
+#  else
+#     puts "The line is currently empty."
+#  end
+#end
 
-def take_a_number(katz_deli, new_customer)
-  katz_deli.push(new_customer)
-  last_in_line_number = katz_deli.index(new_customer)+1
-  last_in_line_name = katz_deli.last
-  puts "Welcome, #{last_in_line_name}. You are number #{last_in_line_number} in line."
+def take_a_number(katz_deli)
+  
+# katz_deli is now an array of ticket numbers, not names
+
+  $ticket_number += 1 
+  puts "#{$something}, you are ticket number #{$ticket_number}. You are number #{$number_in_line} in line."
+  $number_in_line += 1
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length > 0 
-    puts "Currently serving #{katz_deli.shift}."
+  if $serving_counter > 0  
+    puts "Currently serving #{$serving_counter}."
+    $serving_counter += 1 
+    $number_in_line -= 1
+    
   else
     puts "There is nobody waiting to be served!"
   end
 end
+
+line = []
+take_a_number(line)
+# Welcome, you are ticket number 1. You are number 1 in line.
+take_a_number(line)
+# Welcome, you are ticket number 2. You are number 2 in line.
+now_serving(line)
+now_serving(line)
+take_a_number(line)
+now_serving(line)
+take_a_number(line)
+take_a_number(line)
+take_a_number(line)
