6b1c9b30ff9d05f4b3f13e4dd816b1715e5a187a
Kat B <katreesebarnes@gmail.com>
2019-02-12 17:12:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -41,6 +41,22 @@ end
      
  end 
  
+ def now_serving(katz_deli)
+   
+ if  katz_deli.length == 0 
+   
+   puts "There is nobody waiting to be served!"
+   
+ else 
+   
+      name = katz_deli.shift
+      
+      puts "Currently serving #{name}."
+      
+    end
+   
+ end
+ 
   
   
 
