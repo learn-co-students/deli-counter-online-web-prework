208924d025a56ee52b184f35758728bfc703ccae
Kat B <katreesebarnes@gmail.com>
2019-02-12 02:17:11 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,39 @@
-# Write your code here.
+ require 'pry'
+  
+def line(katz_deli)
+  
+ if  katz_deli.length == 0
+  
+   
+  puts "The line is currently empty."
+  
+else 
+  people_in_line = "The line is currently:"
+  
+  katz_deli.each_with_index do |name, index| 
+    people_in_line << " #{index + 1}. #{name}"
+    
+  end
+  
+  puts people_in_line
+  
+  
+end
+
+ end
+ 
+ def take_a_number(katz_deli, message)
+   
+   
+   
+   katz_deli.include?("Ada")
+   
+   puts "Welcome, #{"
+     
+   end
+   
+   
+ end
+ 
+
+
