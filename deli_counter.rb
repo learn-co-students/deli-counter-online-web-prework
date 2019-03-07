458d79ee4df198fd26f2b4c39e9c49a7d65d0800
Mark Kanko <mkanko66@gmail.com>
2019-03-07 16:07:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 def line(deli_line)
-  if deli_line.length == 0 
+  if deli_line.length == 0
     puts "The line is currently empty."
-  else 
-    message_string = "The line is currently:" 
-    deli_line.each_with_index {|item, index| message_string << " #{index + 1}. #{item}"}  
-    puts message_string 
+  else
+    message_string = "The line is currently:"
+    deli_line.each_with_index {|item, index| message_string << " #{index + 1}. #{item}"}
+    puts message_string
   end
 end
 
@@ -12,32 +12,26 @@ def take_a_number(deli_line, name)
   n = deli_line.length + 1
   deli_line.push(name)
   puts "Welcome, #{name}. You are number #{n} in line."
-end  
+end
 
 def now_serving(deli_line)
-  if deli_line.length == 0 
+  if deli_line.length == 0
     puts "There is nobody waiting to be served!"
   else puts "Currently serving #{deli_line.shift}."
- end 
-end 
-  
+ end
+end
 
-
-
-
-
-
-
+# deli_line. length == 0 ? "There is nobody waiting to be served!" : "Currently serving #{deli_line.shift}."
 
 #def line(deli_line)
-  
+
 #  katz_deli_line = Array.new
-  
-#  if deli_line == 0 
+
+#  if deli_line == 0
 #    puts "The line is currently empty."
-#  else deli_line.each_with_index {|name, index| katz_deli_line.push "The line is currently: #{index + 1}. #{name}"} 
-#    return katz_deli_line 
-#  end 
-#end 
+#  else deli_line.each_with_index {|name, index| katz_deli_line.push "The line is currently: #{index + 1}. #{name}"}
+#    return katz_deli_line
+#  end
+#end
 
   
