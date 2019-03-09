5d1836bbb1b4c76e5b4d4a952499bcb02257b8d4
Patrick A <akolo@hotmail.com>
2019-03-09 21:07:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+# Write your code here
+def Deli_Count( Katz_deli, other_deli, another_deli )
+   Katz_deli = Array.new 
+   
+    while Katz_deli == []
+      puts "The Line is currently empty"
+    end 
+  
+  
+end
