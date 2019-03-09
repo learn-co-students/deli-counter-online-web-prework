726eba801ac0a856a3a1c0ea832cdcb598d98d65
Kyle Graden <kylegraden3@gmail.com>
2019-03-09 05:31:43 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.size <= 0
+    puts "The line is currently empty."
+  else
+    puts "The line is currently:" + katz_deli.each_with_index do |name, index|
+      index_plus_one = index_plus_one
+      puts" #{index_plus_one}. #{name}")
+    end
+  end
+end
