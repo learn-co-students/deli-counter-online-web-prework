533fdb6db6c7b50f299a93e7aed6813e48413e07
Ibrahim  Akar <ibsakar@gmail.com>
2019-03-27 16:59:08 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+katz_deli = [] 
+
+def line 
+
+
+
+ if line is empty 
+  put "The line is currently empty."
+end 
+
+ puts persons name 
+ 
+ Top-Tip: Remember that people like to count from 1
+ 
+ now_serving 
+ 
+ puts 
+ 
+ next in line 
+ 
+ puts "nobody in line"
+ 
+ end 
