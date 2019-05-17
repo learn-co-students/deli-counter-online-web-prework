ec71b7474b7409bbbe45d479d75562b20c752b66
Jack Atherton <jackrrye@gmail.com>
2019-05-17 19:40:40 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+require "pry"
+katz_deli=[]
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else 
+    array = katz_deli.each_with_index do |name, index| 
+      "#{index}. #{name}"
+    end
+    binding.pry 
+    puts "The line is currently: #{array}."
+  end
+end
+
+
+
+
+
+
