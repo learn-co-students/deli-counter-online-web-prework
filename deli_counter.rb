c521437f79372f1e1404f0cf573f89257f31dc21
Huy Do <huyddo40@gmail.com>
2019-05-21 22:35:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,26 @@
 def line(arr)
   if !arr.empty?
-		puts "The line is currently:"
-		puts "#{arr.index(name)+1}. #{name}"
-			
+		print "The line is currently: "
+		arr.each_with_index do |name, index|
+		  print index+1,". ",name, " "
 		end
-	
-  else
-		puts "The line is currently empty"
+	else
+		puts "The line is currently empty."
 	end
+end
+
+def take_a_number(arr,name)
+	arr.push(name)
+	puts "Welcome #{name}. You are number #{arr.index(name)+1} in line."
+end
+
+def now_serving(arr)
+	if !arr.empty?
+		puts "Currently serving #{arr[0]}"
+		arr.shift()
+	else
+		puts "There is nobody witing to be served!"
+  end
 end
 
 def take_a_number(arr,name)
