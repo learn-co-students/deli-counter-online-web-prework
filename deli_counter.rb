2f6237fa730a36c30c5dc15d13514d34d8028ac6
kpobrien4 <48386395+kpobrien4@users.noreply.github.com>
2019-06-02 20:02:42 -0400

    Update deli_counter.rb
    Created the deli counter

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length >= 1
+    nuarray = []
+    counter = 1
+    array.each do |name|
+      nuarray.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{nuarray.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+line(katz_deli)
+
+def take_a_number(line,new_person)
+  line.push(new_person)
+    puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end
+
+def now_serving(katz_deli)
+  if line.length == 0
+     puts"There is nobody waiting to be served!"
+   else
+     puts "Currently serving #{line[0]}."
+     line.shift
+   end
+ end
+
+
+puts now_serving(katz_deli)
+puts katz_deli
