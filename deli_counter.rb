d59ac24f46ffc080ec92062a5737f61db9c598b8
Shaneen Muhammad <mshaneen.sm@gmail.com>
2019-06-08 04:01:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+katz_deli[]
+
+def line 
+  katz_deli[]
+  puts "The line is empty"
+end 
+
+def take_a_number(katz_deli,"new_name")
+  katz_deli = ["person 1", "person"]
+  katz_deli << .index("#{new_name}")
+  puts "#{name} has number"
+end
+
+im stuck
+  
